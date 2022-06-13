f473a562e9be0479268e4e360bcaa2e2b1cc17fe
Neil Tendolkar <neiltendolkar10@gmail.com>
2022-06-13 13:25:04 -0700

    Game Loop/Logistics

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, os
 
 pygame.font.init()
 pygame.mixer.init()
+pygame.display.init()
 
 # Window/Screen
 # https://stackoverflow.com/questions/6104207/what-exactly-does-pygame-display-set-mode-do
@@ -32,3 +33,34 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 # User Events
 SHIP_1_HIT = pygame.USEREVENT + 1
 SHIP_2_HIT = pygame.USEREVENT + 2
+
+# Assets
+ORANGE_SHIP_IMG = pygame.image.load(os.path.join('Assets', 'Orange_Spaceship.png'))
+GREEN_SHIP_IMG = pygame.image.load(os.path.join('Assets', 'Green_Spaceship.png'))
+SPACE = pygame.image.load(os.path.join('Assets', 'Space_Background.jpg'))
+# Assign game objects based on style
+SHIP_1_IMG = ORANGE_SHIP_IMG
+SHIP_2_IMG = GREEN_SHIP_IMG
+BACKGROUND = pygame.transform.scale(SPACE, (WIDTH, HEIGHT))
+
+# Principal Functions
+# https://realpython.com/lessons/using-blit-and-flip/
+def draw_window():
+    pass
+
+
+# main Definition
+def main():
+    # Running Loop
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        # Update all parameters
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+# main Execution
+main()
